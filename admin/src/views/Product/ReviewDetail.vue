f083cb536b6ca7daf2df861d1daaa7b7888af287
dai-haraki <dai-haraki@tayutau.co.jp>
2018-10-02 10:11:05 +0900

    sampleを一部削除

--- admin/src/views/Product/ReviewDetail.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="review-detail">
-    <h1>レビュー情報</h1>
-    <!-- 実装方法は商品情報とだいたい同じ -->
-  </div>
-</template>
--- admin/src/views/ProductList.1.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="product-list">
-    <h1>商品一覧</h1>
-  </div>
-</template>
